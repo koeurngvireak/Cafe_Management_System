cb3193c681e6a3a4012679f413a9b5750f36db16
koeurngvireak <vireakkoeurng6327@gmail.com>
2025-06-19 10:13:53 +0700

    added

--- Coffee_System_App/models.py
+++ Coffee_System_App/models.py
@@ -1,30 +1,30 @@
-from django.db import models
+# from django.db import models
 
-# Create your models here.
-class drink(models.Model):
-    drink_id = models.AutoField(primary_key=True)
-    drink_name = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=5, decimal_places=2)
-    qty = models.IntegerField(default=0)
-    total_price = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
-    image = models.ImageField(upload_to='images/', default='images/default.jpg')
-    def __str__(self):
-        return self.drink_name
-    class customer(models.Model):
-        customer_N = models.AutoField(primary_key=True)
-        customer_id = models.CharField(max_length=10, unique=True)
-        total_drinks = models.IntegerField(default=0)
-        total_price = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
-        status = models.CharField(max_length=20, default='Pending')
-        def __str__(self):
-            return self.customer_id
+# # Create your models here.
+# class drink(models.Model):
+#     drink_id = models.AutoField(primary_key=True)
+#     drink_name = models.CharField(max_length=100)
+#     price = models.DecimalField(max_digits=5, decimal_places=2)
+#     qty = models.IntegerField(default=0)
+#     total_price = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+#     image = models.ImageField(upload_to='images/', default='images/default.jpg')
+#     def __str__(self):
+#         return self.drink_name
+#     class customer(models.Model):
+#         customer_N = models.AutoField(primary_key=True)
+#         customer_id = models.CharField(max_length=10, unique=True)
+#         total_drinks = models.IntegerField(default=0)
+#         total_price = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+#         status = models.CharField(max_length=20, default='Pending')
+#         def __str__(self):
+#             return self.customer_id
         
-    class order_details(models.Model):
-        order_id = models.AutoField(primary_key=True)
-        drink = models.ForeignKey(drink, on_delete=models.CASCADE)
-        price = models.DecimalField(max_digits=5, decimal_places=2)
-        qty = models.IntegerField(default=1)
-        total = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
+#     class order_details(models.Model):
+#         order_id = models.AutoField(primary_key=True)
+#         drink = models.ForeignKey(drink, on_delete=models.CASCADE)
+#         price = models.DecimalField(max_digits=5, decimal_places=2)
+#         qty = models.IntegerField(default=1)
+#         total = models.DecimalField(max_digits=5, decimal_places=2, default=0.00)
 
-        def __str__(self):
-            return f"Order {self.order_id} for {self.qty} x {self.drink.drink_name}"
+#         def __str__(self):
+#             return f"Order {self.order_id} for {self.qty} x {self.drink.drink_name}"
